12e072aade59e3af15fd40741144e73b0e90bbc1
juanescorreap <juanestebancope@outlook.com>
2022-11-29 19:43:44 -0500

    task 7

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,25 +1,7 @@
-num getPoints(Map team) {
-  num points = 0;
-  team.forEach((key, value) {
-    if (key == 'Free throws') {
-      points += value;
-    } else if (key == '2 pointer') {
-      points += value * 2;
-    } else if (key == '3 pointer') {
-      points += value * 3;
-    }
-  });
-  return points;
-}
-
-num whoWins(Map teamA, Map teamB) {
-  num teamAPoints = getPoints(teamA);
-  num teamBPoints = getPoints(teamB);
-  if (teamAPoints > teamBPoints) {
-    return 1;
-  } else if (teamAPoints < teamBPoints) {
-    return 2;
-  } else {
-    return 0;
-  }
+int whoWins(Map<String, int> team1, Map<String, int> team2) {
+  var team1Total = team1['Free throws']! + team1['2 pointers']! * 2 + team1['3 pointers']! * 3;
+  var team2Total = team2['Free throws']! + team2['2 pointers']! * 2 + team2['3 pointers']! * 3;
+  if (team1Total > team2Total) return 1;
+  if (team1Total < team2Total) return 2;
+  return 0;
 }
